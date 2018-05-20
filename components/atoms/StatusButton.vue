09e01faf6953168faa5538cbbe1d5de098dabbb8
k-okina <okinakahiro@gmail.com>
2018-05-20 11:02:57 +0900

    [add] ステータスボタンatom作成

---
+++ components/atoms/StatusButton.vue
@@ -0,0 +1,81 @@
+<template>
+  <div
+    @click="$emit('click', true)"
+    :style="{ 'background-color': color }"
+    :class="{ animation }"
+    class="c-a-status-button"
+  >
+    <div
+      :class="{ animation }"
+      class="c-a-status-button-context">
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StatusButton',
+  props: {
+    color: {
+      type: String,
+      default: 'rgb(196, 196, 196)',
+    },
+    size: {
+      type: Number,
+      default: 12,
+    },
+    animation: {
+      type: Boolean,
+      default: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.c-a-status-button {
+  --height: 32px;
+
+  position: relative;
+  height: 100%;
+  cursor: pointer;
+  user-select: none;
+
+  &:hover {
+    transition: filter 0.3s ease;
+    filter: opacity(70%);
+  }
+}
+
+.c-a-status-button-context {
+  height: 100%;
+  line-height: var(--height);
+  border-bottom-style: solid;
+  border-bottom-width: 1px;
+  padding: 0 5px;
+  color: #fff;
+  border-radius: 0;
+  text-align: center;
+  font-size: 12px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  vertical-align: baseline;
+}
+
+.c-a-status-button.animation {
+  animation: show .3s;
+}
+
+@keyframes show {
+  0% {
+    flex: 0 0 16px;
+    width: 16px;
+  }
+  100% {
+    width: 100%;
+    flex: 0 0 120px;
+  }
+}
+</style>
--- stories/index.js
+++ stories/index.js
@@ -2,26 +2,42 @@ import { storiesOf } from '@storybook/vue'
 import VueInfoAddon from 'storybook-addon-vue-info'
 import { withKnobs, text, number, boolean } from '@storybook/addon-knobs'
 import { withNotes } from '@storybook/addon-notes'
-import { withReadme, withDocs, doc } from 'storybook-readme';
+import { withReadme, withDocs, doc } from 'storybook-readme'
+import SimpleStatusButton from '@/components/atoms/StatusButton.vue'
 import StatusButton from '@/components/molecules/StatusButton.vue'
+import NewStatusPicker from '@/components/organisms/NewStatusPicker.vue'
 import MoleculesCatalog from '@/stories/molecules/catalog.md'
-import '!style-loader!css-loader!~/assets/main.scss'
+import '!style-loader!css-loader!~assets/stylesheet/style.scss'
 
+
+storiesOf('Atoms', module)
+  .add('catalog', () => ({
+    components: { SimpleStatusButton },
+    template: `
+    <div style="display: flex;">
+      <div style="width: 140px; height: 35px; margin-right: 20px;">
+        <SimpleStatusButton>Sample</SimpleStatusButton>
+      </div>
+    </div>`,
+  }))
 
 storiesOf('Molecules', module)
   .addDecorator(withDocs(MoleculesCatalog))
   .add('catalog', () => ({
     components: { StatusButton },
     data() {
-      return { size: 18, continue: true }
+      return { size: 18 }
     },
     template: `
     <div style="display: flex;">
       <div style="width: 140px; height: 35px; margin-right: 20px;">
         <StatusButton/>
       </div>
+      <div style="width: 140px; height: 35px; margin-right: 20px;">
+        <StatusButton color="bisque" :size="size" continue>Sample</StatusButton>
+      </div>
       <div style="width: 140px; height: 35px;">
-        <StatusButton color="bisque" :size="size">Sample</StatusButton>
+        <StatusButton color="blue" :size="size">Long Text 1 2 3 4 5 6 7 8 9 0</StatusButton>
       </div>
     </div>`,
   }));
