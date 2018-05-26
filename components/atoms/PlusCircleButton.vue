f45a7b64702adcfea46db552729f2ed7f1e5d9ab
k-okina <okinakahiro@gmail.com>
2018-05-26 13:45:44 +0900

    [add] カーソルを合わせるとプラスが浮き出る円形のボタンを作成

---
+++ components/atoms/PlusCircleButton.vue
@@ -0,0 +1,47 @@
+<template>
+<div>
+  <i
+    @mouseenter="focus()"
+    @mouseleave="blur()"
+    @click="$emit('click', $event)"
+    :style="{ color }"
+    :class="{ 'fa-plus-circle': isFocus, 'fa-circle': !isFocus }"
+    class="fas"
+  ></i>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'PlusCircleButton',
+  props: {
+    color: {
+      type: String,
+      default: 'black',
+    },
+  },
+  data() {
+    return {
+      isFocus: false,
+    };
+  },
+  methods: {
+    focus() {
+      this.isFocus = true
+      this.$emit('focus')
+    },
+    blur() {
+      this.isFocus = false
+      this.$emit('blur')
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.fas {
+  display: block;
+  border-radius: 50%;
+  cursor: pointer;
+}
+</style>
