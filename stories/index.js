9f43199dcec6a98b8965062c6b5fcd8440bff772
k-okina <okinakahiro@gmail.com>
2018-05-26 16:56:36 +0900

    [add] ストーリー修正

--- stories/index.js
+++ stories/index.js
@@ -58,24 +58,37 @@ storiesOf('Organisms', module)
   .add('catalog', () => ({
     components: { StatusPicker, StatusPickerEditer },
     data: () => ({
-      buttons: [
+      statuses: [
         { id: 0, order: 2, name: 'Done', color: 'rgb(0, 200, 117)', canDelete: true },
         { id: 1, order: 3, name: 'Working on it', color: 'rgb(253, 171, 61)', canDelete: true },
-        { id: 2, order: 1, name: 'Stuck', color: 'rgb(226, 68, 92)', canDelete: false },
-        { id: 3, order: 4, name: 'Waiting for Approval', color: 'rgb(0, 200, 117)', canDelete: false },
-        { id: 4, order: 5, name: '', color: 'rgb(196, 196, 196)', canDelete: true },
       ],
       colors: [
-        'rgb(0, 200, 117)',
+        'rgb(226, 68, 93)',
         'rgb(253, 171, 61)',
-        'rgb(226, 68, 92)',
+        'rgb(0, 200, 117)',
+        'rgb(0, 134, 192)',
+        'rgb(3, 127, 76)',
+        'rgb(202, 182, 65)',
+        'rgb(255, 203, 0)',
+        'rgb(51, 51, 51)',
+        'rgb(87, 155, 252)',
+        'rgb(162, 93, 220)',
+        'rgb(187, 51, 84)',
+        'rgb(255, 21, 138)',
+        'rgb(255, 90, 196)',
+        'rgb(120, 75, 209)',
+        'rgb(156, 211, 38)',
+        'rgb(102, 204, 255)',
+        'rgb(128, 128, 128)',
+        'rgb(127, 83, 71)',
+        'rgb(255, 100, 46)',
         'rgb(196, 196, 196)',
       ],
     }),
     template: `
     <div>
-      <StatusPicker style="margin-top: 30px;"/>
+      <StatusPicker style="margin-top: 30px;" animation/>
       <hr style="margin: 20px 0">
-      <StatusPickerEditer :buttons="buttons" :colors="colors"/>
+      <StatusPickerEditer :statuses="statuses" :colors="colors"/>
     </div>`,
   }))
